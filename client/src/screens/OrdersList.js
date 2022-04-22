47d8fc717ca2752e7dd2e9daf0dd8ffa1d019729
aayush1017 <d.aayushjain@gmail.com>
2022-04-23 04:09:07 +0530

    Added ordersList

--- client/src/screens/OrdersList.js
+++ client/src/screens/OrdersList.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Error from "../components/Error";
+import Filter from "../components/Filter";
+import Loading from "../components/Loading";
+import { getAllOrders } from "../actions/orderAction";
 
 function OrdersList() {
+  const dispatch = useDispatch();
+
+  const getordersstate = useSelector((state) => state.getAllOrdersReducer);
+  const { loading, error, orders } = getordersstate;
+
+  useEffect(() => {
+    dispatch(getAllOrders());
+  }, []);
   return (
     <div>
-        <h1>Orders List</h1>
+      {loading && <Loading />}
+      {error && <Error error="Something went wrong" />}
+      <table className="table table-striped table-bordered">
+        <thead className="table-dark">
+          <tr>
+            <th>Order Id</th>
+            <th>Email</th>
+            <th>User Id</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders && orders.map(order => {
+            return (
+              <tr>
+                <td>{order._id}</td>
+                <td>{order.email}</td>
+                <td>{order.userid}</td>
+                <td>{order.orderamount}</td>
+                <td>{order.createdAt.substring(0,10)}</td>
+                <td>Status</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default OrdersList
+export default OrdersList;
