5a4315c4dacf7bb57e50120dfd9d702b6f5022b4
aayush1017 <d.aayushjain@gmail.com>
2022-04-22 17:29:58 +0530

    Edit Pizza

--- client/src/screens/EditPizza.js
+++ client/src/screens/EditPizza.js
@@ -1,20 +1,22 @@
 import React, { useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
+import { useParams } from 'react-router-dom'
 import { getPizzaById } from '../actions/pizzaAction'
 
-function Editpizza({match}) {
+export default function Editpizza({match}) {
+
+    const {pizzaid=""} = useParams()
+
     const dispatch = useDispatch()
     useEffect(() => {
-      dispatch(getPizzaById(match?.params?.pizzaid))
+      dispatch(getPizzaById(pizzaid))
     }, [])
-    
+    console.log(pizzaid)
   return (
     <div>
         <h1>Edit Pizza</h1>
-        <h1>Pizza Id = {match?.params?.pizzaid}</h1> 
-        {/* ABove code not working */}
+        <h1>Pizza Id = {pizzaid}</h1> 
+        {/* Above code not working */}
     </div>
   )
 }
-
-export default Editpizza
